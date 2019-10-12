b7bd860d50c94fa887e425e0f9d2b5df16e1eb28
AlexeyDer <hottelmich@gmail.com>
2019-10-13 00:08:50 +0700

    Add class MyRectangle with constructor

--- src/main/java/MyRectangle.java
+++ src/main/java/MyRectangle.java
@@ -1,2 +1,25 @@
-public class MyRectangle {
+public class MyRectangle extends MyTriangle {
+    private int x3, y3;
+
+    MyRectangle(int x, int y, int x1, int y1, int x2, int y2, int x3, int y3) {
+        super(x, y , x1, y1, x2, y2);
+        setX3(x3);
+        setY3(y3);
+    }
+
+    public int getX3() {
+        return x3;
+    }
+
+    public int getY3() {
+        return y3;
+    }
+
+    public void setX3(int x3) {
+        this.x3 = x3;
+    }
+
+    public void setY3(int y3) {
+        this.y3 = y3;
+    }
 }
