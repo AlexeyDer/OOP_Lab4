d642c8904712fe7b99d957a9d364c89fb599bc02
AlexeyDer <hottelmich@gmail.com>
2019-10-13 00:21:30 +0700

    Add classes Circle and Elips with constuctors

--- src/main/java/MyCircle.java
+++ src/main/java/MyCircle.java
@@ -1,3 +1,25 @@
-public class MyCircle {
+public class MyCircle extends tPoint {
+    private int width, len;
 
+    protected MyCircle(int x, int y, int width, int len) {
+        super(x, y);
+        setLen((len));
+        setWidth(width);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLen() {
+        return len;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
 }
--- src/main/java/MyElips.java
+++ src/main/java/MyElips.java
@@ -1,2 +1,5 @@
-public class MyElips {
+public class MyElips extends MyCircle {
+    protected MyElips(int x, int y, int width, int len) {
+        super(x, y, width, len);
+    }
 }
